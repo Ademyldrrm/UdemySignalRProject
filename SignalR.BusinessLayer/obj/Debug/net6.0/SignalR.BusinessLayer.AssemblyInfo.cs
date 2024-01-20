1593563c0153f08ba75db101a4eee726979968b5
Adem Yıldırım <yldrmadem52@gmail.com>
2024-01-20 23:38:08 +0300

    76.Ders Tamamlandı

--- SignalR.BusinessLayer/obj/Debug/net6.0/SignalR.BusinessLayer.AssemblyInfo.cs
+++ SignalR.BusinessLayer/obj/Debug/net6.0/SignalR.BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b03bd9b07ecbf238136a69abd42945876d3000c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1217a3ae69870f68b58bc19d63095100c00f7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SignalR.WebUI/obj/Debug/net6.0/SignalR.WebUI.AssemblyInfo.cs
+++ SignalR.WebUI/obj/Debug/net6.0/SignalR.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b03bd9b07ecbf238136a69abd42945876d3000c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1217a3ae69870f68b58bc19d63095100c00f7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
